053e69dda3abfd2656a2c86a05c6d87af4d5a9f5
Carl <carlyip36@gmail.com>
2024-05-24 10:44:17 +0800

    Implemented Home, All Courses, Course Overview Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,13 +15,13 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
 
   <!-- CSS Implementing Plugins -->
-  <link rel="stylesheet" href="../assets/vendor/bootstrap-icons/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="../assets/vendor/hs-mega-menu/dist/hs-mega-menu.min.css">
-  <link rel="stylesheet" href="../assets/vendor/swiper/swiper-bundle.min.css">
+  <link rel="stylesheet" href="{{asset('frontend/vendor/bootstrap-icons/font/bootstrap-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/vendor/hs-mega-menu/dist/hs-mega-menu.min.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/vendor/swiper/swiper-bundle.min.css')}}">
 
 
   <!-- CSS Front Template -->
-  <link rel="stylesheet" href="../assets/css/theme.min.css">
+  <link rel="stylesheet" href="{{asset('frontend/css/theme.min.css')}}">
 </head>
 
 <body>
@@ -1843,19 +1843,19 @@
   <!-- ========== END SECONDARY CONTENTS ========== -->
 
   <!-- JS Global Compulsory  -->
-  <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('frontend/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- JS Implementing Plugins -->
-  <script src="../assets/vendor/hs-header/dist/hs-header.min.js"></script>
-  <script src="../assets/vendor/hs-mega-menu/dist/hs-mega-menu.min.js"></script>
-  <script src="../assets/vendor/hs-show-animation/dist/hs-show-animation.min.js"></script>
-  <script src="../assets/vendor/hs-go-to/dist/hs-go-to.min.js"></script>
-  <script src="../assets/vendor/typed.js/lib/typed.min.js"></script>
-  <script src="../assets/vendor/fslightbox/index.js"></script>
-  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{asset('frontend/vendor/hs-header/dist/hs-header.min.js')}}"></script>
+  <script src="{{asset('frontend/vendor/hs-mega-menu/dist/hs-mega-menu.min.js')}}"></script>
+  <script src="{{asset('frontend/vendor/hs-show-animation/dist/hs-show-animation.min.js')}}"></script>
+  <script src="{{asset('frontend/vendor/hs-go-to/dist/hs-go-to.min.js')}}"></script>
+  <script src="{{asset('frontend/vendor/typed.js/lib/typed.min.js')}}"></script>
+  <script src="{{asset('frontend/vendor/fslightbox/index.js')}}"></script>
+  <script src="{{asset('frontend/vendor/swiper/swiper-bundle.min.js')}}"></script>
 
   <!-- JS Front -->
-  <script src="../assets/js/theme.min.js"></script>
+  <script src="{{asset('frontend/js/theme.min.js')}}"></script>
 
   <!-- JS Plugins Init. -->
   <script>
